593fffac855694744d237c83b3723c2a3a8cd636
Andrew Matthews <matthews.andrew@gmail.com>
2023-10-30 11:14:59 +1100

    allow action as last step

---
+++ ActorSrcGen.Proxies.Playground/MyWorkflow2.cs
@@ -0,0 +1,25 @@
+﻿namespace ActorSrcGen.Abstractions.Playground;
+
+[Actor]
+public partial class MyWorkflow2
+{
+    [InitialStep(next: "DoTask2")]
+    public string DoTask1(int x)
+    {
+        Console.WriteLine("DoTask1");
+        return x.ToString();
+    }
+
+    [Step(next: "DoTask3")]
+    public string DoTask2(string x)
+    {
+        Console.WriteLine("DoTask2");
+        return $"100{x}";
+    }
+
+    [LastStep]
+    public void DoTask3(string input)
+    {
+        Console.WriteLine("DoTask3");
+    }
+}

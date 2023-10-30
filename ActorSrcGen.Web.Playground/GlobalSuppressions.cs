a25d2e79fff0d90048520f1188d8a1ea03049c56
Andrew Matthews <matthews.andrew@gmail.com>
2023-10-30 11:21:55 +1100

    some cleanup

--- ActorSrcGen.Web.Playground/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "HAA0101:Array allocation for params parameter", Justification = "<Pending>", Scope = "type", Target = "ActorSrcGen.Web.Playground.Controllers.CalcController")]
-[assembly: SuppressMessage("Performance", "HAA0601:Value type to reference type conversion causing boxing allocation", Justification = "<Pending>", Scope = "type", Target = "ActorSrcGen.Web.Playground.Controllers.CalcController")]

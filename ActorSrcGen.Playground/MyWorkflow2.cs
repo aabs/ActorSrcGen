e0e077a452250e13a059b992b93fdfdb4f3acb80
Andrew Matthews <matthews.andrew@gmail.com>
2024-04-24 11:45:52 +1000

    filtering on method lookups + TLEH

--- ActorSrcGen.Playground/MyWorkflow2.cs
+++ ActorSrcGen.Playground/MyWorkflow2.cs
@@ -1,25 +1,29 @@
 ﻿namespace ActorSrcGen.Abstractions.Playground;
 
 [Actor]
-public partial class MyWorkflow2
+public partial class MyActor
 {
+    public List<int> Results { get; set; } = [];
     [InitialStep(next: "DoTask2")]
-    public string DoTask1(int x)
+    public Task<string> DoTask1(int x)
     {
         Console.WriteLine("DoTask1");
-        return x.ToString();
+        return Task.FromResult(x.ToString());
     }
 
     [Step(next: "DoTask3")]
-    public string DoTask2(string x)
+    public Task<string> DoTask2(string x)
     {
         Console.WriteLine("DoTask2");
-        return $"100{x}";
+        return Task.FromResult($"100{x}");
     }
 
     [LastStep]
-    public void DoTask3(string input)
+    public Task<int> DoTask3(string input)
     {
         Console.WriteLine("DoTask3");
+        int result = int.Parse(input);
+        Results.Add(result);
+        return Task.FromResult(result);
     }
 }

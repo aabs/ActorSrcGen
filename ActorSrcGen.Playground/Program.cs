af8133734280cee2be3396681ce93c81bedd3c8b
Andrew Matthews <matthews.andrew@gmail.com>
2024-04-24 12:13:08 +1000

    simplified filter logic

--- ActorSrcGen.Playground/Program.cs
+++ ActorSrcGen.Playground/Program.cs
@@ -1,10 +1,12 @@
 ﻿using ActorSrcGen.Abstractions.Playground;
 
+Console.WriteLine("Hello World");
+
 var wf = new MyWorkflow();
 if (await wf.Cast(10))
     Console.WriteLine("Called Asynchronously");
 
-var wf2 = new MyWorkflow2();
+var wf2 = new MyActor();
 if (wf2.Call(10))
     Console.WriteLine("Called Synchronously");
 

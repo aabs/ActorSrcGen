e3ea07ef1103a6faa84dac8ad59068488afaeec0
Andrew Matthews <matthews.andrew@gmail.com>
2024-04-28 13:43:38 +1000

    receiver functions

--- ActorSrcGen.Playground/Program.cs
+++ ActorSrcGen.Playground/Program.cs
@@ -2,12 +2,12 @@
 
 Console.WriteLine("Hello World");
 
-var wf = new MyWorkflow();
-if (await wf.Cast(10))
-    Console.WriteLine("Called Asynchronously");
-
-var wf2 = new MyActor();
-if (wf2.Call(10))
+var actor = new MyActor();
+if (actor.Call(10))
     Console.WriteLine("Called Synchronously");
 
-await wf2.CompletionTask;
+await actor.SignalAndWaitForCompletionAsync();
+
+var result = await actor.ReceiveDoTask3Async(CancellationToken.None);
+
+Console.WriteLine($"Result: {result}");

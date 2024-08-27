83ef1795b9051cbf63ffb56352bb23f1ff0d032b
Andrew Matthews <A30006806@agl.com.au>
2024-08-27 12:59:30 +1000

    Add MyActor class, update version, enhance ReadMe, add image
    Introduced a new `MyActor` class in `MyActor.cs` with methods to handle a dataflow pipeline, including `ReceivePollRequest`, `DecodeRequest`, `ActOnTheRequest`, `DeliverResults`, and helper methods.
    
    Added a new `PollResults` record to `MyActor.cs`.
    
    Updated the version in `Directory.Build.props` from `2.0.0` to `2.1.0`.
    
    Enhanced `ReadMe.md` with detailed usage instructions for the ActorSrcGen library, including installation steps, examples, and explanations of the generated code. Removed outdated examples and added new ones.
    
    Added a new PNG image file `file1.png` with all necessary chunks and data.

--- ActorSrcGen.Playground/MyActor.cs
+++ ActorSrcGen.Playground/MyActor.cs
@@ -1,5 +1,6 @@
 ﻿using Gridsum.DataflowEx;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace ActorSrcGen.Abstractions.Playground;
 
@@ -8,6 +9,60 @@ public record Context<TRequestType, TPayloadType>(TRequestType OriginalRequest,
     Stack<IDisposable> Activities);
 
 public record PollRequest(string Id, string Name);
+public record PollResults(string Id, double[] Values);
 
 public record DataPoint(DateTimeOffset Timestamp, double Value);
 public record TelemetryResponse(string Id, string Name, DataPoint[] Result);
+
+
+[Actor]
+public partial class MyActor
+{
+    [Ingest(1)]
+    [NextStep(nameof(DecodeRequest))]
+    public async Task<string> ReceivePollRequest(CancellationToken cancellationToken)
+    {
+        return await GetTheNextRequest();
+    }
+
+    [FirstStep("decode incoming poll request")]
+    [NextStep(nameof(ActOnTheRequest))]
+    public PollRequest DecodeRequest(string json)
+    {
+        Console.WriteLine(nameof(DecodeRequest));
+        var pollRequest = JsonSerializer.Deserialize<PollRequest>(json);
+        return pollRequest;
+    }
+
+    [Step]
+    [NextStep(nameof(DeliverResults))]
+    public PollResults ActOnTheRequest(PollRequest req)
+    {
+        Console.WriteLine(nameof(ActOnTheRequest));
+        var result = GetTheResults(req.Id);
+        return result;
+    }
+
+    [LastStep]
+    public bool DeliverResults(PollResults res)
+    {
+        return TryPush(res);
+    }
+
+    private bool TryPush(PollResults res)
+    {
+        return true;
+    }
+
+    private async Task<string> GetTheNextRequest()
+    {
+        return """
+               {"property":  "value"}
+               """;
+    }
+
+    private PollResults GetTheResults(string id)
+    {
+        return new PollResults(id, [1,2,3,4,5]);
+    }
+}
